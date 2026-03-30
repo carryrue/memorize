92bc121fd393be81801a5886a8aaaf02b8c277fa
carryrue <benson101016@gmail.com>
2026-03-30 17:22:01 +0800

    feat:lazyvgrid

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,22 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis: Array<String> = ["A","B","C","D","Z","T","P","H","O","M","Q","W","E","F","S","Y","G"]
-    @State var emojiCount = 6
+    var emojis: Array<String> = ["A","B","C","D","Z","T","P","H","O","M","Q","W","E","F","S","Y","🐮"]
+    @State var emojiCount = 7
     var body: some View {
         VStack{
-            HStack {
-                ForEach(emojis[0..<emojiCount], id: \.self){emoji in CardView(content: emoji)
-                }
-            }
-            HStack {
-                remove
-                Spacer()
-                add
-            }
+            cardlist
+            Spacer()
+            actionButtons
         }
         .padding()
         .foregroundStyle(.orange)
+    }
+    var cardlist: some View{
+        ScrollView{
+            LazyVGrid(columns:[GridItem(.adaptive(minimum: 85),spacing: 0.01)],spacing: 0.01){
+                ForEach(emojis[0..<emojiCount], id: \.self){ emoji in CardView(content: emoji)
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+        }
+    }
+    var actionButtons: some View{
+        HStack{
+            remove
+            Spacer()
+            add
+        }
     }
     var remove: some View{
         Button{
@@ -53,15 +63,17 @@ struct CardView :View {
     var content: String
     var body: some View {
         ZStack {
-            var shape=RoundedRectangle(cornerRadius: 20.0)
-            if isFaceup {
+            let shape=RoundedRectangle(cornerRadius: 20.0)
+            Group{
                 shape.fill(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(content)
+                    .font(Font.system(size: 300))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1,contentMode: .fit)
             }
-            else{
-                RoundedRectangle(cornerRadius: 20.0)
-            }
+            .opacity(isFaceup ? 1 : 0)
+            shape.opacity(isFaceup ? 0 : 1)
         }
         .onTapGesture(perform: {
             isFaceup = !isFaceup
